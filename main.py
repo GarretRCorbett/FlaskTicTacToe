978e1ac11ea2e16e803375c5ff2913079da46e4e
unknown <garretrcorbett@gmail.com>
2022-11-06 23:26:34 -0500

    playing around.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("main.py")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/hello/<name>")
+def hello(name=None):
+
+    return render_template('hello.html', name=name.title())
